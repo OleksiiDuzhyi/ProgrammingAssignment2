bcd7bb7d91120dba406d18b0a6a3322b8854e0f9
oleksiiduzhyi <oleksiiduzhyi@gmail.com>
2015-08-21 23:16:50 +0200

    Implement cache matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## creates a list (kind of an object in OOP) which holds matrix and
+## inverse matrix. Whenever matrix is reset - inverse matrix is set 
+## to NULL. To access a matrix use $get() list element, to set a 
+## matrix usr $set(matrix) list element. To set and get inverted 
+## matrix use $setInverted(matrix) and $getInverted() correspondingly
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(matrix) {
+        x <<- matrix
+        inverse <<- NULL
+    }
+    get <- function(){
+        x
+    }
+    setInverse <- function(solve) {
+        inverse <<- solve
+    }
+    getInverse <- function() {
+        inverse
+    }
+    list(set = set, get = get, 
+         setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculates an inverse matrix for a given cacheMatrix holder.
+## If inverse matrix was calculated before - simply returns 
+## cached value, otherwise - calculating the inverse matrix
+## and caches (sets) it to a cacheMatrix holder
+## NOTE: non-invertable matricies are not handeled at this version
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if (!is.null(inverse)) {
+        message("using cached value")
+        return(inverse)
+    }
+    matrix <- x$get()
+    inverse <- solve(matrix)
+    x$setInverse(inverse)
+    inverse
 }
